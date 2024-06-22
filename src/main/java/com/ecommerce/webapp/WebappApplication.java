0e160476aa288dedc87aa458b94fb99d526aea4f
MinaSameh1 <minasameh1@gmail.com>
2024-06-22 23:21:51 +0300

    Enable mongodb repos

--- src/main/java/com/ecommerce/webapp/WebappApplication.java
+++ src/main/java/com/ecommerce/webapp/WebappApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class WebappApplication {
 
 	public static void main(String[] args) {
